1032251c9a58b1abb96b2aecbb6f880c2b109cbc
selee <oksktmddjs@gmail.com>
2025-03-13 01:12:54 +0900

    First Commit

---
+++ src/main/java/LSE/HealthPower/HealthPowerApplication.java
@@ -0,0 +1,13 @@
+package LSE.HealthPower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthPowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthPowerApplication.class, args);
+	}
+
+}
